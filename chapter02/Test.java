4ae88934e0bd207023f16667b177472ce5d4736f
StanLong <youlinbhz@sina.cn>
2021-07-11 20:00:38 +0800

    object类

--- chapter02/Test.java
+++ chapter02/Test.java
@@ -1,26 +1,12 @@
 /*
-  接口也是一种引用类型，可以等同的看作类
-  接口是一种特殊的抽象类，接口是完全抽象的，只有抽象方法和常量
-  接口没有构造方法，不能被实例化
-  接口和接口之间可以多继承
-  一个类可以实现多个接口
+  hashCode
 */
 
-/*public class Test{
+public class Test{
     public static void main(String[] args){
+        // hashCode 返回的是该对象的哈希码值
+        // java对象的内存地址是经过哈希算法得出的int类型的数值
+        Test t = new Test();
+        System.out.println(t.hashCode());
     }
-}*/
-
-public interface Test{
-    public static final String SUCCESS = "success";
-    
-    // public static final可以省略
-    String FAILED = "failed";
-    
-    public abstract void m1();
-    
-    // public abstract 是可以省略的
-    void m2();
-    
-    
 }
