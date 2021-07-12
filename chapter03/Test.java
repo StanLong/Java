db394e0e07443528900455115850d078d1c5b569
StanLong <youlinbhz@sina.cn>
2021-07-13 00:23:55 +0800

    异常

---
+++ chapter03/Test.java
@@ -0,0 +1,14 @@
+/*
+  异常：
+  异常模拟的是现实世界中不正常的事件
+  Java中采用类的形式模拟异常
+  
+*/
+
+public class Test{
+    
+    
+    public static void main(String[] args){
+        System.out.println("系统已安全退出！");
+    }
+}
