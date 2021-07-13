b30265223159953a8dc81147e21427152fa406b2
StanLong <youlinbhz@sina.cn>
2021-07-13 22:37:40 +0800

    异常

--- chapter03/Test.java
+++ chapter03/Test.java
@@ -1,14 +1,45 @@
 /*
-  异常：
-  异常模拟的是现实世界中不正常的事件
-  Java中采用类的形式模拟异常
-  
+  自定义异常
+    1. 编译时异常 直接继承 Exception
+    2. 运行时异常 直接继承 RuntimeException
 */
 
 public class Test{
     
-    
     public static void main(String[] args){
-        System.out.println("系统已安全退出！");
+        String username = "jack";
+        CustomerService cs = new CustomerService();
+        try{
+            cs.register(username);
+        }catch(IllegalNameException e){
+            System.out.println(e.getMessage());
+        }
+        
+    }
+    
+    
+}
+
+// 编译时异常
+class IllegalNameException extends Exception{
+    
+    // 定义异常一般提供两个构造方法
+    public IllegalNameException(String msg){
+        super(msg);
+    }
+}
+
+class CustomerService{
+    public void register(String name) throws IllegalNameException{
+        if (name.length() < 6){
+            // 创建异常对象
+            //IllegalNameException e = new IllegalNameException("用户名长度不能少于6位");
+            // 手动抛出异常
+            // throw e;
+            
+            //可简写
+            throw new IllegalNameException("用户名长度不能少于6位");
+        }
+        System.out.println("注册成功");
     }
 }
