190734174f7e14ac2834f2770661e66f890f8345
StanLong <youlinbhz@sina.cn>
2021-07-17 14:06:57 +0800

    常用类

--- chapter05/Test.java
+++ chapter05/Test.java
@@ -1,25 +1,31 @@
 /*
-    正则表达式
+    包装类型
+    基本数据类型	包装类型
+    byte	        Byte
+    boolean	        Boolean
+    short	        Short
+    char	        Character
+    int	            Integer
+    long	        Long
+    float	        Float
+    double	        Double
 */
-import java.lang.StringBuffer;
-import java.lang.StringBuilder;
 public class Test{
     
+    // 需求：规定m1方法可以接受java中任何一种数据类型
+    // Object只能接受基本数据类型，不能接收引用数据类型，
+    // 所以m1方法如果想接受byte类型的数据，可以将byte类型先包装成java.lang.Byte， 再传递参数
+    public static void m1(Object o){
+        System.out.println(o);
+    }
+    
     public static void main(String[] args){
-        // 需求 校验qq号
-        // 要求， 长度5-15, 不能以0开头，只能是数字
+        // 基本数据类型
+        byte b = 10;
         
-        String qq = "a1534824780";
+        // 引用类型
+        Byte b1 = new Byte(b);
         
-        // 校验规则
-        String regex = "[1-9][0-9]{4,14}";
-        
-        boolean flag = qq.matches(regex);
-        
-        if(flag){
-            System.out.println("校验成功");
-        }else{
-            System.out.println("校验失败");
-        }
+        m1(b);
     }
 }
