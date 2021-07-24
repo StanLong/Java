24482b848b6b63651d3910a3ac781433f960762e
StanLong <youlinbhz@sina.cn>
2021-07-24 17:23:51 +0800

    IO流

--- chapter07/Test.java
+++ chapter07/Test.java
@@ -1,38 +1,28 @@
 /*
-    带缓冲区的字节字符输入输出流
-    字节
-    BufferedInputStream
-    BufferedOutputStream
+    递归找出指定目录下的所有目录及文件
     
-    字符
-    BufferedReader
-    BufferedWriter
-    
-    转换流
-    InputStreamReader
 */
 
 import java.io.*;
 public class Test{
     
     public static void main(String[] args) throws Exception{
-        BufferedReader br = null;
-            
-        /*FileInputStream fis = new FileInputStream("Test.java"); // 文件字节输入流
+        String path = "D:/StanLong/git_repository/Java";
+        File file = new File(path);
+        m1(file);
+    }
+    
+    public static void m1(File f){
         
-        InputStreamReader isr = new InputStreamReader(fis); // 将字节流转换成字符流
-       
-        br = new BufferedReader(isr);*/
-        
-        // 简写
-        br = new BufferedReader(new InputStreamReader(new FileInputStream("Test.java")));
-        
-        // 开始读
-        String temp = null;
-        while((temp = br.readLine()) != null){ // br.readLine() 行尾不带换行符
-            System.out.println(temp);
+        if(f.isFile()){
+            return;
         }
         
-        br.close(); // 使用了装饰者模式，这里只要关闭最外层的流即可
+        File[] fs = f.listFiles();
+        
+        for(File subFile : fs){
+            System.out.println(subFile.getAbsolutePath());
+            m1(subFile);
+        }
     }
 }
