3b5a713b00a4e0f1c177bd41ec869fcc4215a044
StanLong <youlinbhz@sina.cn>
2021-07-27 23:23:33 +0800

    注解

--- chapter09/Test.java
+++ chapter09/Test.java
@@ -1,23 +1,11 @@
 /*
-    反射获取父类和父接口
+    注解
 */
 
 import java.lang.reflect.*;
 
 public class Test{
   public static void main(String[] args) throws Exception{
-        Class c = Class.forName("java.lang.String");
-    
-        //获取父类
-        Class superClass = c.getSuperclass();
         
-        System.out.println(superClass.getName());
-        System.out.println("================");
-            
-        //获取父接口
-        Class[] ins = c.getInterfaces();
-        for(Class in:ins) {
-            System.out.println(in.getName());
-        }
     }
 }
